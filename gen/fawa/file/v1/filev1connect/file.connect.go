6d99ab69038e249baee79e4b530a7f7fb9686851
whosefriendA <whosefrienda@163.com>
2025-06-21 23:42:38 +0800

    fix: proto path wrong
    Signed-off-by: whosefriendA <whosefrienda@163.com>

--- gen/fawa/file/v1/filev1connect/file.connect.go
+++ gen/fawa/file/v1/filev1connect/file.connect.go
@@ -22,7 +22,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/fawa-io/fawa/gen/proto/fawa/file/v1"
+	v1 "github.com/fawa-io/fawa/gen/fawa/file/v1"
 	http "net/http"
 	strings "strings"
 )
--- gen/fawa/greet/v1/greetv1connect/hello.connect.go
+++ gen/fawa/greet/v1/greetv1connect/hello.connect.go
@@ -22,7 +22,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/fawa-io/fawa/gen/proto/fawa/greet/v1"
+	v1 "github.com/fawa-io/fawa/gen/fawa/greet/v1"
 	http "net/http"
 	strings "strings"
 )
