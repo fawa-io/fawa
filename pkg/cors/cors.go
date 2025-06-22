832939c2e68decb039594d4439a44a0a439b581c
Gang Wang <144648286+whosefriendA@users.noreply.github.com>
2025-06-23 00:40:20 +0800

    feat: add file web (#24)
    * feat: add file web
    
    Signed-off-by: whosefriendA <whosefrienda@163.com>
    
    * update .gitignore
    
    * optimize code style
    
    * opitmzie code style
    
    ---------
    
    Signed-off-by: whosefriendA <whosefrienda@163.com>
    Co-authored-by: marsevilspirit <marsevilspirit@gmail.com>

--- pkg/cors/cors.go
+++ pkg/cors/cors.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package cors
 
 import (
 	"net/http"
@@ -21,7 +21,7 @@ import (
 	"github.com/rs/cors"
 )
 
-func newCORS() *cors.Cors {
+func NewCORS() *cors.Cors {
 	// To let web developers play with the demo service from browsers, we need a
 	// very permissive CORS setup.
 	return cors.New(cors.Options{
