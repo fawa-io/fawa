5f516d43d2c8c728672f5ffd6925027aa6746631
whosefriendA <whosefrienda@163.com>
2025-07-26 22:22:18 +0800

    feat: new canva

---
+++ Newcanva/pkg/cors/cors.go
@@ -0,0 +1,25 @@
+package cors
+
+import (
+	"net/http"
+)
+
+type CORS struct{}
+
+func NewCORS() *CORS {
+	return &CORS{}
+}
+
+func (c *CORS) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
