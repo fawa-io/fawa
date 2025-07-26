888c86cf7aeb488978f769be2599c8ef217fb0e6
whosefriendA <whosefrienda@163.com>
2025-07-26 13:27:29 +0800

    fix:fix err

--- k6_test/script.js
+++ k6_test/script.js
@@ -1,3 +1,17 @@
+// Copyright 2025 The fawa Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
@@ -14,9 +28,8 @@ export const options = {
   // 定义通过/失败的阈值
   thresholds: {
     // 要求95%的请求延迟必须在500ms以内
-    'http_req_duration': ['p(95)<500'], 
     // 要求99%的请求延迟必须在1500ms(1.5s)以内
-    'http_req_duration': ['p(99)<1500'],
+    'http_req_duration': ['p(95)<500', 'p(99)<1500'],
   },
 };
 
