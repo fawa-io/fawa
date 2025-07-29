eb18378158e17645758e21afb7f00bf32e3b33f9
whosefriendA <whosefrienda@163.com>
2025-07-29 18:08:59 +0800

    refactor:change to microservices

---
+++ services/fileservice/go.mod
@@ -0,0 +1,47 @@
+module github.com/fawa-io/fawa/services/fileservice
+
+go 1.24.5
+
+require (
+	connectrpc.com/connect v1.18.1
+	github.com/fawa-io/fawa v0.2.0
+	github.com/fawa-io/fwpkg v0.0.0-20250729040635-e49839d3bf75
+	github.com/fsnotify/fsnotify v1.9.0
+	github.com/go-redis/redismock/v9 v9.2.0
+	github.com/minio/minio-go/v7 v7.0.95
+	github.com/redis/go-redis/v9 v9.11.0
+	github.com/spf13/pflag v1.0.7
+	github.com/spf13/viper v1.20.1
+	google.golang.org/protobuf v1.36.6
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/minio/crc64nvme v1.0.2 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+	github.com/rs/xid v1.6.0 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tinylib/msgp v1.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ services/fileservice/main.go
@@ -0,0 +1,89 @@
+// Copyright 2025 The fawa Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/fawa-io/fwpkg/cors"
+	"github.com/fawa-io/fwpkg/fwlog"
+
+	"github.com/fawa-io/fawa/services/fileservice/config"
+	"github.com/fawa-io/fawa/services/fileservice/gen/file/v1/filev1connect"
+	file "github.com/fawa-io/fawa/services/fileservice/handler"
+)
+
+func main() {
+	if err := config.InitConfig(); err != nil {
+		fwlog.Fatalf("Failed to initialize configuration: %v", err)
+	}
+
+	cfg := config.Get()
+
+	logLevel, err := fwlog.ParseLevel(cfg.LogLevel)
+	if err != nil {
+		fwlog.Warnf("Invalid initial log level '%s': %v. Using default.", cfg.LogLevel, err)
+	}
+	fwlog.SetLevel(logLevel)
+	fwlog.Infof("Logger initialized with level: %s", cfg.LogLevel)
+
+	fileSvcHdr := &file.FileServiceHandler{}
+	fileProcedure, fileHandler := filev1connect.NewFileServiceHandler(fileSvcHdr)
+
+	mux := http.NewServeMux()
+	mux.Handle(fileProcedure, fileHandler)
+
+	fileSrv := &http.Server{
+		Addr:    cfg.Addr,
+		Handler: cors.NewCORS().Handler(mux),
+	}
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+
+		fwlog.Info("Shutting down server...")
+
+		// Close file service
+		if err := fileSvcHdr.Close(); err != nil {
+			fwlog.Errorf("Error closing file service: %v", err)
+		}
+
+		// Set timeout for HTTP server shutdown
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := fileSrv.Shutdown(ctx); err != nil {
+			fwlog.Errorf("Server shutdown error: %v", err)
+		}
+
+		fwlog.Info("Server shutdown complete")
+		os.Exit(0)
+	}()
+
+	fwlog.Infof("Server starting on %v", cfg.Addr)
+
+	// Start the HTTPS server.
+	if err := fileSrv.ListenAndServeTLS(cfg.CertFile, cfg.KeyFile); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		fwlog.Fatalf("Failed to start server: %v", err)
+	}
+}
